2bc8a4c6abbb744ad71d65ceacc8f0a6c08a11ea
Maikoncarlos <maikonsaturno@gmail.com>
2023-03-11 17:48:45 -0300

    retirando imports não usados reclamados pelo SonarLint

--- src/main/java/com/github/maikoncarlos/viacep/controllers/mappers/AddressControllerMapper.java
+++ src/main/java/com/github/maikoncarlos/viacep/controllers/mappers/AddressControllerMapper.java
@@ -3,10 +3,8 @@ package com.github.maikoncarlos.viacep.controllers.mappers;
 import com.github.maikoncarlos.viacep.controllers.dtos.response.AddressResponseDto;
 import com.github.maikoncarlos.viacep.services.domain.AddressResponseDomain;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper (componentModel = "spring")
 public interface AddressControllerMapper {
-
         AddressResponseDto toResponseDto(AddressResponseDomain responseDomain);
 }
--- src/main/java/com/github/maikoncarlos/viacep/services/mapper/AddressServiceMapper.java
+++ src/main/java/com/github/maikoncarlos/viacep/services/mapper/AddressServiceMapper.java
@@ -4,11 +4,9 @@ import com.github.maikoncarlos.viacep.clients.entity.AddressResponseEntity;
 import com.github.maikoncarlos.viacep.services.domain.AddressResponseDomain;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface AddressServiceMapper {
-
     @Mapping ( target = "locality" , source = "localidade" )
     @Mapping ( target = "abbreviationCity" , source = "uf" )
     AddressResponseDomain toDomain(AddressResponseEntity addressEntity);
