b70ae00dda3cd8f5611a9d3871eac09329b892c0
Maikoncarlos <maikonsaturno@gmail.com>
2023-04-08 16:02:08 -0300

    ajuste na validação dos testes da classe AddressServiceImpl

--- src/test/java/com/github/maikoncarlos/viacep/services/AddressServiceImplTest.java
+++ src/test/java/com/github/maikoncarlos/viacep/services/AddressServiceImplTest.java
@@ -1,12 +1,10 @@
 package com.github.maikoncarlos.viacep.services;
 
 import com.github.maikoncarlos.viacep.repository.AddressRepository;
-import com.github.maikoncarlos.viacep.repository.AddressSalvedRepository;
 import com.github.maikoncarlos.viacep.repository.entity.AddressResponseEntity;
 import com.github.maikoncarlos.viacep.services.domain.AddressResponseDomain;
 import com.github.maikoncarlos.viacep.services.exceptions.AddressNotFound;
 import com.github.maikoncarlos.viacep.services.mapper.AddressServiceMapper;
-import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,9 +15,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith (MockitoExtension.class)
 @Slf4j
@@ -28,7 +25,7 @@ class AddressServiceImplTest {
     public static final String ZIPCODE = "08140060";
     public static final String STREET = "Rua Antonio";
     public static final String NEIGHBORTHOOD = "Itaim Paulista";
-    public static final String LOCAIDADE = "Sao Paulo";
+    public static final String LOCALIDADE = "Sao Paulo";
     public static final String UF = "SP";
     private static final Long ID = 1L;
     @InjectMocks
@@ -38,7 +35,7 @@ class AddressServiceImplTest {
     @Mock
     private AddressServiceMapper serviceMapper;
     @Mock
-    private AddressSalvedRepository addressSalvedRepository;
+    private SalvedAddressService salvedAddressService;
 
     private AddressResponseEntity addressResponseEntity;
     private AddressResponseDomain addressResponseDomain;
@@ -54,14 +51,21 @@ class AddressServiceImplTest {
     @DisplayName ("deve obter o endereço pelo cep e devolver com sucesso")
     void getAddressByZipCodeWithSucess() {
         when(addressRepository.getingAddressByZipcodeInViaCEP(ZIPCODE)).thenReturn(addressResponseEntity);
+        doNothing().when(salvedAddressService).salvedAddress(addressResponseEntity);
         when(serviceMapper.toDomain(addressResponseEntity)).thenReturn(addressResponseDomain);
 
         AddressResponseDomain responseDomain = assertDoesNotThrow(() -> addressService.getAddressByZipCode(ZIPCODE));
 
         assertNotNull(responseDomain);
-        assertDoesNotThrow(()-> addressRepository.getingAddressByZipcodeInViaCEP(any()));
         assertEquals(ZIPCODE, responseDomain.getZipcode());
         assertEquals(STREET, responseDomain.getStreet());
+        assertEquals(NEIGHBORTHOOD, responseDomain.getNeighborhood());
+        assertEquals(LOCALIDADE, responseDomain.getLocality());
+        assertEquals(UF, responseDomain.getAbbreviationCity());
+
+        verify(addressRepository, times(1)).getingAddressByZipcodeInViaCEP(eq(ZIPCODE));
+        verify(salvedAddressService, times(1)).salvedAddress(eq(addressResponseEntity));
+        verify(serviceMapper, times(1)).toDomain(eq(addressResponseEntity));
 
     }
 
@@ -71,11 +75,16 @@ class AddressServiceImplTest {
         when(addressRepository.getingAddressByZipcodeInViaCEP(anyString()))
                 .thenReturn(AddressResponseEntity.builder().build());
 
-        assertThrows(AddressNotFound.class, ()-> addressService.getAddressByZipCode(anyString()));
+        AddressNotFound exception = assertThrows(AddressNotFound.class,
+                () -> addressService.getAddressByZipCode(anyString()));
+
+        assertEquals("Endereço não encontrado - cep invãlido!", exception.getMessage());
+        verify(salvedAddressService, times(0)).salvedAddress(eq(addressResponseEntity));
+        verify(serviceMapper, times(0)).toDomain(eq(addressResponseEntity));
     }
 
     private void addressEntity() {
-        addressResponseEntity = new AddressResponseEntity(ID, ZIPCODE, STREET, "", NEIGHBORTHOOD, LOCAIDADE, UF);
-        addressResponseDomain = new AddressResponseDomain(ZIPCODE, STREET, "", NEIGHBORTHOOD, LOCAIDADE, UF);
+        addressResponseEntity = new AddressResponseEntity(ID, ZIPCODE, STREET, "", NEIGHBORTHOOD, LOCALIDADE, UF);
+        addressResponseDomain = new AddressResponseDomain(ZIPCODE, STREET, "", NEIGHBORTHOOD, LOCALIDADE, UF);
     }
 }
